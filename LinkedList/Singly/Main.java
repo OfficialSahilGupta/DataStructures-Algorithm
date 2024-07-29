fe2429a5482f5a04202234194bf313be8e1f4243
user.name = "OfficialSahilGupta <sahilgup.official@gmail.com>
2024-07-29 22:02:55 +0530

    All the files are added successfully

--- LinkedList/Singly/Main.java
+++ LinkedList/Singly/Main.java
@@ -4,28 +4,59 @@ public class Main{
     public static void main(String[] a){
 
         Operations obj = new Operations();
+        // inserting three values at the beginning of the linkedlist.
+        System.out.println("12, 13 & 14 inserted in the first place in linkedlist.");
         obj.insertBegin(14);
         obj.insertBegin(13);
         obj.insertBegin(12);
-        obj.display();
 
+        // this method will traverse the linkedlist when obj.display() method invoked
+        obj.display();
+        System.out.println();
+
+        // inserting three values at the last of the linkedlist.
+        System.out.println("90, 100 & 110 inserted end at the linkedlist respectively");
         obj.insertEnd(90);
         obj.insertEnd(100);
         obj.insertEnd(110);
         obj.display();
+        System.out.println();
 
+        // inserting two values at the index 3, 5 & 7 respectivelyof the linkedlist.
+        System.out.println("15, 95 & 105 inserted at the index 3, 5 & 7 respectively.");
         obj.insertIndex(15,3);
         obj.insertIndex(95,5);
         obj.insertIndex(105,7);
         obj.display();
+        System.out.println();
 
+        // deleting the value "12" at the beginning at the linkedlist.
+        System.out.println("12 was deleted at the first position.");
         obj.deleteFirst();
         obj.display();
+        System.out.println();
 
+        // deleting the value "90" at the last of the linkedlist.
+        System.out.println("110 was deleted at the last position.");
         obj.deleteEnd();
         obj.display();
-
+        System.out.println();
+        
+        // deleting 15 at the index of 2 in the linkedlist.
+        System.out.println("15 was deleted at the index 2.");
         obj.deleteIndex(2);
         obj.display();
+        System.out.println();
+
+        // Search for a value in the linked list
+        System.out.println("Searching for value 40: " + obj.search(40));
+        System.out.println();
+
+        System.out.println("Searching for value 95: " + obj.search(95));
+        System.out.println();
+
+        // Get the length of the linked list
+        System.out.println("Length of the linked list: " + obj.length());
+        System.out.println();
     }
 }
--- LinkedList/Singly/Operation/Operations.java
+++ LinkedList/Singly/Operation/Operations.java
@@ -1,28 +1,37 @@
 package Operation;
+
+// declaration of the public class Operations having all the required opeartions for singly linkedlist.
 public class Operations{
+    // so it applied on all the nodes
     private int size;
     private Node head;
     private Node tail;
 
+    // this is our user defined datatype which is Node made from private class
     private class Node{
         private Node next;
         private int value;
 
+        // this constructor initialized the value.
         public Node(int value){
             this.value = value;
         }
 
+        // this constructor initializes the value and reference next.
         public Node(int value, Node next){
             this.value = value;
             this.next = next;
         }
     }
 
+    // this constructor initialize size of 0.
     public Operations(){
         this.size = 0;
     }
 
+    // this method will calls when required for the insertion at the beginning.
     public void insertBegin(int value){
+        // this edge case refers that if tail is null then tail = head;
         if(tail == null){
             tail = head;
         }
@@ -34,6 +43,7 @@ public class Operations{
 
     }
 
+    // this method will calls when required for the insertion at the end.
     public void insertEnd(int value){
         if(tail == null){
             insertBegin(value);
@@ -46,12 +56,15 @@ public class Operations{
         size++;
     }
 
+    // this method will calls when required for the insertion at the specific position in the sigly linkedlist
     public void insertIndex(int value, int index){
+        // thie edge case says when the index is at 0th position calls insertFirst(value) method.
         if(index == 0){
             insertBegin(value);
             return;
         }
 
+        // thie edge case says when the index is at the last position calls insertEnd(value) method.
         if(index == size){
             insertEnd(value);
             return;
@@ -67,6 +80,7 @@ public class Operations{
         size++;
     }
 
+    // this method will calls when required for the deletion at the beginning.
     public int deleteFirst(){
         int val = head.value;
         head = head.next;
@@ -75,7 +89,9 @@ public class Operations{
         return val;
     }
 
+    // this method will calls when required for the deletion at the end.
     public int deleteEnd(){
+        // thie edge case says when the index is at the first position calls insertFirst(value) method.
         if(size <= 1){
             return deleteFirst();
         }
@@ -97,11 +113,13 @@ public class Operations{
     }
 
     public int deleteIndex(int index){
+        // thie edge case says when the index is at the firsst position calls insertFirst(value) method
         if(index == 0){
             return deleteFirst();
         }
 
         if(index == size){
+            // thie edge case says when the index is at the last position calls insertEnd(value) method
             return deleteEnd();
         }
 
@@ -112,7 +130,8 @@ public class Operations{
 
         return val;
     }
-
+    
+    // this method will tranverse the linkedlist
     public void display(){
         Node temp = head;
         while(temp != null){
@@ -120,5 +139,22 @@ public class Operations{
             temp = temp.next;
         }
         System.out.println("END");
+    }
+
+    // method to search for a given value in the linkedlist or not.
+    public boolean search(int value){
+        Node temp = head;
+        while(temp != null){
+            if(temp.value == value){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    // method to get the lenght of linkedlist.
+    public int length(){
+        return size;
     }
 }
